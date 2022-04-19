7a330d3f25b25b192a46a38146bea2615817941e
wanjiku <annritawanjiku77@gmail.com>
2022-04-19 16:57:07 +0300

    add custom made pipe for date difference

--- src/app/components/quotes/quotes.component.ts
+++ src/app/components/quotes/quotes.component.ts
@@ -14,7 +14,7 @@ export class QuotesComponent implements OnInit {
   bestquote:number=0;
   
   quotes:Quote[]=[
-    new Quote (0,"A life spent making mistakes is not only more honorable, but more useful than a life spent doing nothing","George Bernhard Shaw","Annie",3,new Date(2022,4,19))
+    new Quote (0,"A life spent making mistakes is not only more honorable, but more useful than a life spent doing nothing","George Bernhard Shaw","Annie",3,new Date(2022,3,13)) //0 is JAN, 1 is FEB...3 is April ..and so on
   ]
   addNewQuote(quote:any){
     let quoteLength=this.quotes.length;
